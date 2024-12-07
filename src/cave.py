9456a0ae2c6627cb5441f05015894555db77cf08
AllanBirrell <L00175861@atu.ie>
2024-12-07 18:17:36 +0000

    style (cave): #6 Fixing the format of the code and comments  #6
    Implented the changes as of the linter, change around the formatting and variable names
    
    PR #18

--- src/cave.py
+++ src/cave.py
@@ -1,23 +1,26 @@
-class cave:
-    #This creates and intialises a cave object if level isn't entered it begins at 0
-    def __init__(self, caveName, caveType, level = 0):
-        self._caveName = caveName
-        self._caveType = caveType
-        self._level = level
-    
+"""This class is to use and create a cave object"""
+class Cave:
+    """This creates and intialises a cave object, if the level isn't entered it begins at 0"""
+
+    def __init__(self, cave_name, cave_type, level = 0):
+        self.cave_name = cave_name
+        self.cave_type = cave_type
+        self.level = level
+
     #Just prints the default statement where player is to be used when they enter a cave
     def __repr__(self):
-        return f"Player has entered: {self._caveName} and it's a {self._caveType} themed cave\nStaring on Floor: {self._level}"
-    
-    #Caves has multiple floors, when new floor entered player is told.
-    def caveDepth(self, level):
-        if level > self._level:
-            self._level = level
+        return f"Player has entered: {self.cave_name} and \
+it's a {self.cave_type} themed cave\nStarting on Floor: {self.level}"
+
+    def cave_depth(self, level):
+        """Caves has multiple floors, when new floor entered player is told."""
+        if level > self.level:
+            self.level = level
             return f"Player has moved up to Floor {level}"
-            
-        elif level < self._level:
-            self._level = level
+
+        if level < self.level:
+            self.level = level
             return f"Player has moved down to Floor {level}"
-            
+
         else:
             return f"Player is currently still on Floor {level}"
---
+++ src/cave_exploration.py
@@ -0,0 +1,7 @@
+from cave import Cave
+"""Tester for the cave class"""
+
+obj = Cave("Temple","Mosque")
+
+print(obj)
+print(obj.cave_depth(1))
